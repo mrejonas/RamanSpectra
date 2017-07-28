a51718b97e391f8bf02402f7139133554248b707
mrejonas <mario.jonas@uct.ac.za>
2017-07-28 11:53:06 +0200

    Script now modifies inout filenames to remove hyphens and pre-pend a text string to the start of names starting with a number; Goldindec runs fail otherwise

--- runner_qsub.pl
+++ runner_qsub.pl
@@ -1,6 +1,26 @@
 #!/usr/bin/perl -w
+$qsub_bash = "#!/bin/bash\n" ;
 
-foreach $prnfilename(@ARGV){
+$qsub_directive = <<'END_DIRECTIVE';
+#PBS -S /bin/bash
+#PBS -q UCTlong
+#PBS -l nodes=2:ppn=4:series600
+#PBS -V
+#PBS -M mario.jonas@uct.ac.za
+#PBS -m ae
+END_DIRECTIVE
+
+$spectrum_num = 1;
+foreach $prnfile(@ARGV){
+  $prnfilename = $prnfile;
+  $prnfilename =~ s/-/_/g;
+  $prnfilename =~ s/\(/_/g;
+  $prnfilename =~ s/\)/_/g;
+  $prnfilename =~ s/\'/_/g;
+  $prnfilename =~ s/ +/_/g;
+  $prnfilename =~ s/\+/postive/g;
+  $prnfilename = "XY_".$prnfilename;
+  system("mv $prnfile $prnfilename");
   $baselineoutfile = $prnfilename; $baselineoutfile =~ s/\..+$/_baseline\.csv/;
   $txtfilename = $prnfilename; $txtfilename =~ s/\.prn/\.txt/;
   $trimmedtext = $prnfilename; $trimmedtext =~ s/\.prn/_trimmed\.txt/;
@@ -20,27 +40,26 @@ foreach $prnfilename(@ARGV){
   }
   close(TRIMMEDTXT);
   close(TXTFILE);
-  open(RUNOCTAVE,">$samplename.m");
 
+  open(RUNOCTAVE,">$samplename.m");
   print RUNOCTAVE "addpath ./\n",
         "load $trimmedtext\n",
-        "GoldindecQsub($samplename,4,0.5,0.0001,",
-        "'./',\"$samplename\");";
-  close(RUN);
+        "GoldindecQsub($samplename,4,0.5,0.0001,'./',\"$samplename\");";
+  close(RUNOCTAVE);
 
   open(RUNQSUB,">$samplename.qsub");
-  print RUNQSUB "octave $samplename.m\n",
-          "mv $samplename.csv $baselineoutfile\n",
-          "sed s'/,/\t/' $baselineoutfile > $samplename.temp.csv\n",
-          "rename(\"$samplename.temp.csv\", $baselineoutfile)\n",
-          "paste $trimmedtext $baselineoutfile | awk 'BEGIN {OFS=\"\t\"}{dif=\$2-\$4;print \$1,\$2,\$4,dif}'> $finalout\n";
+  print RUNQSUB $qsub_bash;
+  print RUNQSUB "#PBS -N spectrum$spectrum_num\n";
+  print RUNQSUB $qsub_directive,"\n";
+  print RUNQSUB "module add software/octave-4.0.0\n",
+      "octave --no-gui $samplename.m\n",
+      "sed s'/,/\t/' $samplename.csv > $baselineoutfile\n",
+      "paste $trimmedtext $baselineoutfile | awk 'BEGIN {OFS=\"\t\"}{dif=\$2-\$4;print \$1,\$2,\$4,dif}'> $finalout\n",
+      "rm $samplename.csv $trimmedtext $baselineoutfile $txtfilename\n";
+
   close(RUNQSUB);
 
   system("qsub -d . $samplename.qsub");
-
-  system("time octave $samplename.m");
-  system("mv $samplename.csv $baselineoutfile");
-  system("sed s'/,/\t/' $baselineoutfile > $samplename.temp.csv");
-  rename("$samplename.temp.csv", $baselineoutfile);
-  system("paste $trimmedtext $baselineoutfile | awk 'BEGIN {OFS=\"\t\"}{dif=\$2-\$4;print \$1,\$2,\$4,dif}'> $finalout");
+  $spectrum_num++;
+  if(($spectrum_num % 100) == 0){sleep 300;}
 }
